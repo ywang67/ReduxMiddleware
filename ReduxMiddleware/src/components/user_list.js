6124ef6aa8fd3a4976a54b9959f590c23910a4ba
Your Name <you@example.com>
2017-07-26 15:41:01 -0400

    demo done

---
+++ ReduxMiddleware/src/components/user_list.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
+
+class UserList extends Component{
+
+    componentWillMount(){
+        this.props.fetchUsers();
+    }
+
+    renderUser(user){
+        return(
+            <div className="card card-block" key={user.id}>
+                <h4 className="car-title">{user.name}</h4>
+                <p classsName="card-text">{user.company.name}</p>
+                <a className="btn btn-primary" href={user.website}>Website</a>
+            </div>
+        );
+    }
+
+    render(){
+        return (
+           <div className="user-list">
+               {this.props.users.map(this.renderUser)}
+           </div>
+        );
+    }
+
+}
+
+
+
+const mapStateToProps = (state) =>{
+    return {users: state.users};
+}
+
+export default connect(mapStateToProps, actions)(UserList);
--- ReduxMiddleware/src/index.js
+++ ReduxMiddleware/src/index.js
@@ -5,8 +5,10 @@ import { createStore, applyMiddleware } from 'redux';
 
 import App from './components/app';
 import reducers from './reducers';
+import  Async from './middlewares/async';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(Async)(createStore);
+applyMiddleware(Async,)
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
